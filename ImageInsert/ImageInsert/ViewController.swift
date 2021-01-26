d1cd5d084d705c3b88ddf1a34e21cfe20d216b1b
민성홍 <sunghong32@naver.com>
2021-01-26 12:47:20 +0900

    image insert app

---
+++ ImageInsert/ImageInsert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageInsert
+//
+//  Created by 민성홍 on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
