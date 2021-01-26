ae406277a39ce63adf741a9242484c3837120732
민성홍 <sunghong32@naver.com>
2021-01-26 19:07:27 +0900

    View Transition, UIViewController LifeCycle

---
+++ ViewTransition/ViewTransition/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ViewTransition
+//
+//  Created by 민성홍 on 2021/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController의 view가 메모리에 로드 됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController의 view가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController의 view가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController의 view가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController의 view가 화면에서 사라짐")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("ViewController의 view가 subview를 레이아웃 하려고 함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("ViewController의 view가 subview를 레이아웃 함")
+    }
+
+
+}
+
