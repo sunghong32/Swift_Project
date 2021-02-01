ec8f9cbff91b4562ec2f1e4b426a8b9f1b60ab30
민성홍 <sunghong32@naver.com>
2021-02-01 21:01:00 +0900

    Image Picker App

---
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by 민성홍 on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    lazy var imagePicker: UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func touchUpSelectImageButton(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.image = originalImage
+        }
+        self.dismiss(animated: true, completion: nil )
+    }
+    
+}
+
