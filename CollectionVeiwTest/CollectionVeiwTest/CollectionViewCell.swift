30892a4a3880a20d1ba1c4060da1f4a9ca235b1b
민성홍 <sunghong32@naver.com>
2021-03-18 14:46:05 +0900

    CollectionView Test

---
+++ CollectionVeiwTest/CollectionVeiwTest/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionVeiwTest
+//
+//  Created by 민성홍 on 2021/03/15.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
