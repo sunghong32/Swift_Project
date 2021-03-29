0432c7d0b74907f5516cc097c5895fb22e0d3946
민성홍 <sunghong32@naver.com>
2021-03-29 14:40:17 +0900

    collectionVeiw 테스트

--- CollectionVeiwTest/CollectionVeiwTest/PLViewController.swift
+++ CollectionVeiwTest/CollectionVeiwTest/PLViewController.swift
@@ -1,41 +1,30 @@
 //
-//  ViewController.swift
+//  PLViewController.swift
 //  CollectionVeiwTest
 //
-//  Created by 민성홍 on 2021/03/15.
+//  Created by 민성홍 on 2021/03/19.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    @IBOutlet weak var myCollectionView: UICollectionView!
+class PLViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        myCollectionView.delegate = self
-        myCollectionView.dataSource = self 
+
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
         
-        cell.myLabel.text = "\(indexPath.row + 1)"
-        
-        
-        cell.layer.cornerRadius = 8
-        cell.backgroundColor = UIColor.green
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 300, height: 300)
     }
-    
-    
 }
